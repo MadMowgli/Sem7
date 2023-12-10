3e6711a7db77acdf434342e3cc4bf6d695449d2b
MadMowgli <laeubin.j@gmail.com>
2023-12-10 17:06:00 +0100

    Adding annotation stuff, not done yet

---
+++ src/main/java/ch/laeub/week9/annotations/ToString.java
@@ -0,0 +1,12 @@
+package ch.laeub.week9.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ToString {
+    boolean includeName() default true;
+}
